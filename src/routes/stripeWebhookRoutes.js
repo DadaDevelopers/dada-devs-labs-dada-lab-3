a4f43fd0b1af25e61e28c12db5153b86c46cea23
Ivy-Joy <ivyjoyweda@gmail.com>
2025-12-14 06:52:00 +0300

    Remove the wrongly placed files

--- src/routes/stripeWebhookRoutes.js
+++
@@ -1,5 +0,0 @@
-import express from "express";
-import { stripeWebhookController } from "../controllers/stripeWebhookController.js";
-const router = express.Router();
-router.post("/", stripeWebhookController);
-export default router;
